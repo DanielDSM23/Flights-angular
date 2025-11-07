e8c1467365c7c7d64043708e92c38269191d511b
BrianMad7 <brianmadapathage@gmail.com>
2025-11-07 16:47:34 +0100

    Add flight component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
+import { FlightComponent } from './flight/flight.component';
 
 export const routes: Routes = [
   {
     path: '',
     component : AuthComponent
   },
+  {
+    path: 'flights',
+    component: FlightComponent
+  }
 ];
---
+++ src/app/shared/services/flight.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { FlightInterface } from '../../models/flight.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FlightService {
+
+  constructor(private http: HttpClient) { }
+
+  public getFlights() {
+    return this.http.get<FlightInterface[]>('http://localhost:3000/flights');
+  }
+
+  
+}
