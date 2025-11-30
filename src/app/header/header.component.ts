4073f1abc502db26541b14ffe18cc36cdb3550bd
Daniel MONTEIRO <101821091+DanielDSM23@users.noreply.github.com>
2025-11-30 21:12:08 +0100

    patch user bug

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,8 @@
 import { Component, inject, Input } from '@angular/core';
-import { LucideAngularModule, User } from 'lucide-angular';
+import { LucideAngularModule, User, LogOut } from 'lucide-angular';
 import { Router } from '@angular/router';
 import { SpinLogoDirective } from '../shared/directives/spin-logo.directive';
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -13,11 +14,20 @@ import { SpinLogoDirective } from '../shared/directives/spin-logo.directive';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  isUserLogged = false
+  constructor(private authService: AuthService) {
+    this.isUserLogged = this.authService.isUserLoggedIn();
+  }
   animation = 'animate-pulse'
   readonly user = User;
+  readonly logout = LogOut;
   private router = inject(Router);
   handleSignInClick(){
     this.router.navigate(["/login"])
+  }
+  handleDisconnectClick() {
+    this.authService.logoutUser();
+    location.reload();
   }
 
 }
